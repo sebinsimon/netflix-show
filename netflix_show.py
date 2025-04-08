e0aa30ec27a8a3ae5c7da7b03ae1d0e4aad0543b
Sebin Simon <sebinsimon70@gmail.com>
2025-04-08 18:53:10 +0100

    library update

--- netflix_show.py
+++ netflix_show.py
@@ -1,5 +1,8 @@
 import  pandas as pd
+from pandas import read_csv
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("")
